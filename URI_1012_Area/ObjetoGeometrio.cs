7f5267c3d9950c661c0976685084ac5c18915b01
Joao Milone <joaocmil@hotmail.com>
2021-04-15 12:42:04 -0300

    Update ObjetoGeometrio.cs

--- URI_1012_Area/ObjetoGeometrio.cs
+++ URI_1012_Area/ObjetoGeometrio.cs
@@ -6,10 +6,12 @@ namespace Geometria
     {
         protected string nomeObjeto;
         public abstract double CalcularArea();
+        
         public virtual string ListarArea()
         {
             return CalcularArea().ToString("0.000");
         }
+
         public virtual void DebugArea()
         {
             Console.WriteLine(nomeObjeto + ": " + ListarArea());
