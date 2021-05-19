8031988afaa330ee0815fd9b5b70888b6b6b070d
Joao Milone <joaocmil@hotmail.com>
2021-05-19 18:03:47 -0300

    Added Order System

--- URI_2702_Hard_Choice/OrderSystem.cs
+++ URI_2702_Hard_Choice/OrderSystem.cs
@@ -3,6 +3,72 @@ using System;
 
 namespace OrderSystem
 {
+    public class Order
+    {
+        private List<ItemOrder> _orderList;
 
+        public Order()
+        {
+            _orderList = new List<ItemOrder>();
+        }
 
+        public void AddItemOrder(ItemOrder newItem)
+        {
+            _orderList.Add(newItem);
+        }
+
+        public void PrintTotalWithNoMeal()
+        {
+            Console.WriteLine(QtyPeopleWithNoMeal());
+        }
+
+        int QtyPeopleWithNoMeal()
+        {
+            int sumPeople = 0;
+
+            foreach (ItemOrder item in _orderList)
+                sumPeople += item.QtyPeopleWithNoMeal();
+
+            return sumPeople;
+        }
+    }
+
+    public class ItemOrder
+    {
+        private Item _itemOrder;
+        private int _qtyOrder;
+
+        public ItemOrder(Item newItem, int qtyOrder)
+        {
+            _itemOrder = newItem;
+            _qtyOrder = qtyOrder;
+        }
+
+        int DiferenceBetweenAvailableAndOrderValue => _itemOrder.Qty_Available - _qtyOrder;
+
+        public int QtyPeopleWithNoMeal()
+        {
+            if (DiferenceBetweenAvailableAndOrderValue < 0)
+                return Math.Abs(_itemOrder.Qty_Available - _qtyOrder);
+
+            return 0;
+        }
+    }
+
+    public class Item
+    {
+        private int _qty_Item;
+
+        public Item(int qty)
+        {
+            _qty_Item = qty;
+        }
+
+        public void AddItem(int qtyToAdd)
+        {
+            _qty_Item += qtyToAdd;
+        }
+
+        public int Qty_Available => _qty_Item;
+    }
 }
